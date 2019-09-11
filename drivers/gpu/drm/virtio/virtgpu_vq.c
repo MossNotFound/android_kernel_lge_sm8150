64c0818e4b37e1688f5a77f7f972e50c34bf6b78
David Riley <davidriley@chromium.org>
2019-09-11 11:14:02 -0700

    UPSTREAM: drm/virtio: Rewrite virtio_gpu_queue_ctrl_buffer using fenced version.
    Factor function in preparation to generating scatterlist prior to locking.
    
    Signed-off-by: David Riley <davidriley@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20190911181403.40909-2-davidriley@chromium.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    
    Bug: https://crbug.com/993452
    Change-Id: I6f4348f1cd18752f55a3daefc2c83a8ce699204d
    (cherry picked from commit 32d6c2c5b5227cfc6f796bbd760dab81b05f48e5)
    Signed-off-by: Kaiyi Li <kaiyili@google.com>

--- drivers/gpu/drm/virtio/virtgpu_vq.c
+++ drivers/gpu/drm/virtio/virtgpu_vq.c
@@ -339,17 +339,6 @@ retry:
 	return ret;
 }
 
-static int virtio_gpu_queue_ctrl_buffer(struct virtio_gpu_device *vgdev,
-					struct virtio_gpu_vbuffer *vbuf)
-{
-	int rc;
-
-	spin_lock(&vgdev->ctrlq.qlock);
-	rc = virtio_gpu_queue_ctrl_buffer_locked(vgdev, vbuf);
-	spin_unlock(&vgdev->ctrlq.qlock);
-	return rc;
-}
-
 static int virtio_gpu_queue_fenced_ctrl_buffer(struct virtio_gpu_device *vgdev,
 					       struct virtio_gpu_vbuffer *vbuf,
 					       struct virtio_gpu_ctrl_hdr *hdr,
@@ -392,7 +381,7 @@ again:
 		goto again;
 	}
 
-	if (fence)
+	if (hdr && fence)
 		virtio_gpu_fence_emit(vgdev, hdr, fence);
 	rc = virtio_gpu_queue_ctrl_buffer_locked(vgdev, vbuf, vout);
 	spin_unlock(&vgdev->ctrlq.qlock);
@@ -403,6 +392,12 @@ again:
 	}
 
 	return rc;
+}
+
+static int virtio_gpu_queue_ctrl_buffer(struct virtio_gpu_device *vgdev,
+					struct virtio_gpu_vbuffer *vbuf)
+{
+	return virtio_gpu_queue_fenced_ctrl_buffer(vgdev, vbuf, NULL, NULL);
 }
 
 static int virtio_gpu_queue_cursor(struct virtio_gpu_device *vgdev,
