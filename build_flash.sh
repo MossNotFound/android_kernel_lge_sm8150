97ccf2665d7c2e99ba6e2cbf546b29097303818a
MossNotFound <110248437+MossNotFound@users.noreply.github.com>
2022-08-06 10:53:56 +0700

    script: fix directory not found error and set alpha config as the default
    this should automate the dumb "make release and Dragon folder first before building!" ¬^¬

--- build_flash.sh
+++ build_flash.sh
@@ -13,7 +13,7 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/workspace/proton-clang/bin
+export CLANG_PATH=/home/moss/kernel/proton-clang/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
@@ -21,11 +21,11 @@ export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 echo
 echo "Set DEFCONFIG"
 echo 
-make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out vendor/dragon_flash_defconfig
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out vendor/dragon_alpha_defconfig
 
 echo
 echo "Build The Good Stuff"
 echo 
 
 make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j24
-cp -f ./out/arch/arm64/boot/Image.gz-dtb ./release/Dragon/Image.gz-dtb
+mkdir -p release/Dragon && cp -f ./out/arch/arm64/boot/Image.gz-dtb ./release/Dragon/Image.gz-dtb
